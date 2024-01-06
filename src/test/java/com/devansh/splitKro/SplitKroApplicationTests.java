d8aec7a994924a30283c651ac53d9cbfa12356d0
Devansh Rastogi <rastogidevansh007@gmail.com>
2024-01-06 15:04:24 +0530

    first commit - for splitkro

---
+++ src/test/java/com/devansh/splitKro/SplitKroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devansh.splitKro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitKroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
